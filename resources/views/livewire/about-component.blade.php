4cf42cb3fde76cab0682ba79c6a6faca4dca1f7e
Kusahali <royalrevolt24680@gmail.com>
2024-05-30 03:27:52 +0700

    first commit

---
+++ resources/views/livewire/about-component.blade.php
@@ -0,0 +1,62 @@
+<div>
+<title>Về Chúng Tôi - Cửa Hàng Máy Tính</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 0;
+            
+        }
+        
+        
+        h1, h2 {
+            color: #333;
+        }
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        p {
+            margin-bottom: 15px;
+        }
+        ul {
+            margin-left: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Về Chúng Tôi</h1>
+
+        <h2>Tầm Nhìn và Sứ Mệnh</h2>
+        <p>Tại <strong>Cửa Hàng áo quần</strong>, chúng tôi không chỉ là nơi bán các sản phẩm áo quần, mà còn là người bạn đồng hành đáng tin cậy trên con đường khám phá và chinh phục những trang phục của bạn. Chúng tôi luôn cam kết mang đến những sản phẩm và dịch vụ chất lượng nhất, nhằm thỏa mãn nhu cầu và mong đợi của mọi khách hàng.</p>
+
+        <h2>Giá Trị Cốt Lõi</h2>
+        <ul>
+            <li><strong>Chất Lượng:</strong> Chúng tôi chọn lọc những sản phẩm từ các thương hiệu uy tín, đảm bảo về hiệu suất và độ bền. Tất cả sản phẩm đều được kiểm tra kỹ lưỡng trước khi đến tay khách hàng.</li>
+            <li><strong>Đổi Mới:</strong> Chúng tôi liên tục cập nhật những hàng mới nhất và tiên tiến nhất, giúp bạn luôn dẫn đầu xu hướng và sử dụng những sản phẩm hiện đại nhất.</li>
+            <li><strong>Dịch Vụ Khách Hàng:</strong> Chúng tôi coi trọng mỗi khách hàng và luôn sẵn sàng hỗ trợ, tư vấn nhiệt tình từ khi bạn chọn mua sản phẩm đến khi sử dụng. Chính sách bảo hành và hỗ trợ kỹ thuật của chúng tôi luôn đảm bảo quyền lợi cao nhất cho khách hàng.</li>
+        </ul>
+
+        <h2>Sản Phẩm Của Chúng Tôi</h2>
+        <p>Chúng tôi tự hào cung cấp đa dạng các sản phẩm công nghệ bao gồm:</p>
+        <ul>
+            <li>Áo quần</li>
+            <li>Phụ kiện</li>
+            <li>Giày dép</li>
+        </ul>
+        <h2>Câu Chuyện Của Chúng Tôi</h2>
+        <p>[Thêm câu chuyện ngắn về sự hình thành và phát triển của cửa hàng, những cột mốc quan trọng và những thành tựu đã đạt được.]</p>
+
+        <h2>Liên Hệ Với Chúng Tôi</h2>
+        <p>Chúng tôi luôn mong muốn lắng nghe ý kiến và phản hồi từ bạn. Nếu có bất kỳ câu hỏi hay thắc mắc nào, vui lòng liên hệ với chúng tôi qua:</p>
+        <p><strong>Địa chỉ:</strong>  70 Nguyễn Huệ, Vĩnh Ninh, Thành phố Huế, Thừa Thiên Huế<br>
+        <strong>Số điện thoại:</strong> (+84) 0123456789<br>
+        <strong>Email:</strong> royalrevolt24680@gmail.com<br>
+
+        <p>Cảm ơn bạn đã tin tưởng và lựa chọn <strong>Cửa Hàng áo quần</strong>. Chúng tôi hy vọng sẽ được đồng hành cùng bạn trên hành trình khám phá trang phục mới.</p>
+
+        <p>Trân trọng,<br>
+    </div>
+    </div>
